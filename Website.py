c425b7312c34a282025f175670f2790f323b93cd
naytanExams <natnstgg44@gmail.com>
2025-04-23 11:12:01 +0100

    Update Website.py

--- Website.py
+++ Website.py
@@ -113,3 +113,93 @@ def signup():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+
+
+
+from flask import Flask, render_template, request, redirect, url_for
+import psycopg2
+
+webapp = Flask(__name__)
+webapp.secret_key = "CHANGE_THIS_SECRET"
+
+DB_CONFIG = {
+    "host": "localhost",
+    "dbname": "postgres",
+    "user": "postgres",
+    "password": "postgres",
+    "port": 5432
+}
+
+def validate_login(user, pwd):
+    connection = cursor = None
+    try:
+        connection = psycopg2.connect(**DB_CONFIG)
+        cursor = connection.cursor()
+        query = '''SELECT * FROM "Account_Details" WHERE "Username" = %s AND "Password" = %s'''
+        cursor.execute(query, (user, pwd))
+        return cursor.fetchone()
+    except Exception as err:
+        print(f"Login error: {err}")
+    finally:
+        if cursor:
+            cursor.close()
+        if connection:
+            connection.close()
+
+def register_new_user(fname, lname, email, addr, pcode, uname, pwd):
+    connection = cursor = None
+    try:
+        connection = psycopg2.connect(**DB_CONFIG)
+        cursor = connection.cursor()
+        cursor.execute(
+            '''INSERT INTO "User_Details" ("Forename", "Surname", "Email", "Address", "Post_code") 
+               VALUES (%s, %s, %s, %s, %s) RETURNING "User_ID"''',
+            (fname, lname, email, addr, pcode)
+        )
+        user_id = cursor.fetchone()[0]
+        cursor.execute(
+            '''INSERT INTO "Account_Details" ("Username", "Password", "User_ID") 
+               VALUES (%s, %s, %s)''',
+            (uname, pwd, user_id)
+        )
+        connection.commit()
+    except Exception as err:
+        print(f"Registration error: {err}")
+    finally:
+        if cursor:
+            cursor.close()
+        if connection:
+            connection.close()
+
+@webapp.route('/')
+def main_page():
+    return render_template('Main.html')
+
+@webapp.route('/login', methods=['GET', 'POST'])
+def login_page():
+    if request.method == 'POST':
+        uname = request.form.get('username')
+        pwd = request.form.get('password')
+        if validate_login(uname, pwd):
+            return redirect(url_for('main_page'))
+        return render_template('login.html', error=True)
+    return render_template('login.html')
+
+@webapp.route('/signup', methods=['GET', 'POST'])
+def signup_page():
+    if request.method == 'POST':
+        fname = request.form.get('first_name')
+        lname = request.form.get('last_name')
+        email = request.form.get('email')
+        addr = request.form.get('address')
+        pcode = request.form.get('post_code')
+        uname = request.form.get('username')
+        pwd = request.form.get('password')
+        register_new_user(fname, lname, email, addr, pcode, uname, pwd)
+        return redirect(url_for('login_page'))
+    return render_template('signup.html')
+
+if __name__ == '__main__':
+    webapp.run(debug=True)
